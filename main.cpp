bf289b19a859530ac6eb5b1a4333d22f1f8c1794
JonathanDiaz12 <diaz.j023958@stu.sanjac.edu>
2025-07-07 20:13:14 +0000

    Program that takes input from the user and outputs player stat data for a single player

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+string FirstName;
+string LastName;
+double Score = 0;
+double HoursPlayed = 1;
+double ScorePerHour = 0;
+
+int main()
+{
+    cout << setprecision (2) << fixed;
+    cout<<"Enter the players First Name:"<<endl;
+    cin>> FirstName;
+    cout<<"Enter the players Last Name:"<<endl;
+    cin>>LastName;
+    cout<<"Enter the score of the player:"<<endl;
+    cin>>Score;
+    cout<<"Enter the amount of hours the player played:"<<endl;
+    cin>>HoursPlayed;
+
+    ScorePerHour +=ScorePerHour+(Score/HoursPlayed);
+
+    cout<<"__________"<<endl;
+    cout<<FirstName<<" "<<LastName<<endl;;
+    cout<<"Score: "<<Score<<endl;
+    cout<<"Hours Played: "<<HoursPlayed<<endl;
+    cout<<"Score per hour: "<<ScorePerHour<<endl;
+    cout<<"__________"<<endl;
+
+    return 0;
+}
