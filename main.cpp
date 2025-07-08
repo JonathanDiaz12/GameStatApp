23a9047568a11ae0d89a73a163fbbe9a842fb738
JonathanDiaz12 <diaz.j023958@stu.sanjac.edu>
2025-07-08 15:42:29 +0000

    Fixed the errors in the formula and added comments

--- main.cpp
+++ main.cpp
@@ -2,29 +2,32 @@
 #include <iomanip>
 using namespace std;
 
-
-string FirstName;
-string LastName;
+//Initialize the variables
+string PlayerFirstName;
+string PlayerLastName;
 double Score = 0;
 double HoursPlayed = 1;
 double ScorePerHour = 0;
 
 int main()
 {
+    //This block of code takes input from the user 
     cout << setprecision (2) << fixed;
     cout<<"Enter the players First Name:"<<endl;
-    cin>> FirstName;
+    cin>> PlayerFirstName;
     cout<<"Enter the players Last Name:"<<endl;
-    cin>>LastName;
+    cin>>PlayerLastName;
     cout<<"Enter the score of the player:"<<endl;
     cin>>Score;
     cout<<"Enter the amount of hours the player played:"<<endl;
     cin>>HoursPlayed;
 
-    ScorePerHour +=ScorePerHour+(Score/HoursPlayed);
+    //This is the formula for the score per hour
+    ScorePerHour =(Score/HoursPlayed);
 
+    //outputs the results to the user in a nice format with 2 decimal points.
     cout<<"__________"<<endl;
-    cout<<FirstName<<" "<<LastName<<endl;
+    cout<<PlayerFirstName<<" "<<PlayerLastName<<endl;
     cout<<"Score: "<<Score<<endl;
     cout<<"Hours Played: "<<HoursPlayed<<endl;
     cout<<"Score per hour: "<<ScorePerHour<<endl;
